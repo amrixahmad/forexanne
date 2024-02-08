68c917bb0be231c03e97b3473f4053ecf6130992
amrixahmad <amri.ahmad85@gmail.com>
2024-02-09 00:16:22 +0800

    removed unused slash commands

--- forexgpt/main.py
+++ forexgpt/main.py
@@ -22,22 +22,12 @@ async def on_ready():
     except Exception as e:
         print(e)
 
-@client.tree.command(name="ping",description="it will show the ping")
-async def ping(interaction: Interaction):
-    bot_latency=round(client.latency*1000)
-    await interaction.response.send_message(f"Pong!.. {bot_latency}ms")
-
-@client.tree.command(name="hello",description="say hello to you")
-async def hello(interaction: Interaction):
-    greet = f"Hey {interaction.user.mention}! This is a slash command :)"
-    await interaction.response.send_message(greet)
-
-@client.tree.command(name="forexanne",description="ask Forex Anne anything")
-@app_commands.describe(forex_prompt="Ask me anything about forex")
-async def forexanne(interaction: Interaction,forex_prompt: str):
-    await interaction.response.defer()
-    await asyncio.sleep(delay=6)
-    await interaction.followup.send(chatgpt_response(forex_prompt))
+# @client.tree.command(name="forexanne",description="ask Forex Anne anything")
+# @app_commands.describe(forex_prompt="Ask me anything about forex")
+# async def forexanne(interaction: Interaction,forex_prompt: str):
+#     await interaction.response.defer()
+#     await asyncio.sleep(delay=6)
+#     await interaction.followup.send(chatgpt_response(forex_prompt))
 
 @client.tree.command(name="visionanne",description="send Forex Anne your trade screenshot and question :)")
 @app_commands.describe(trade_ss="Send me your trade screenshot and I will analyze it for you. You can also ask me a question about it if you want :)")
@@ -47,17 +37,5 @@ async def visionanne(interaction: Interaction,trade_ss: discord.Attachment,quest
     image_file = await trade_ss.to_file()
     content = visiongpt_response(trade_ss.url,question)
     await interaction.followup.send(content=content,file=image_file)
-
-
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-    
-#     if message.attachments:
-#         await message.channel.send(visiongpt_response(message.attachments[0].url,message.content))
-    
-#     else:
-#         await message.channel.send(chatgpt_response(message.content))
 
 client.run(FOREXANNE_TOKEN)
